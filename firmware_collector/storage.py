3fd26e7c41fa3c3bf59335661fdabb215e6fbc50
Benjamin Brahmer <info@b-brahmer.de>
2020-09-13 13:39:24 +0200

    migrate a lot to pathlib

--- firmware_collector/storage.py
+++ firmware_collector/storage.py
@@ -3,47 +3,49 @@
 import os
 import re
 import zipfile
+from pathlib import Path
 from firmware_collector.manifest import Manifest
+
 
 class Storage:
     def __init__(self, storage_path):
-        if os.path.isdir(storage_path):
-            self.storage_path = storage_path
-        else:
+        self.storage_path = Path(storage_path)
+        if not self.storage_path.exists():
             raise FileNotFoundError
 
     def save(self, artifact_file):
-
-        #if not os.path.isfile(artifact_file):
-            #return None
-
-        filename = os.path.basename(artifact_file)
+        filename = Path(artifact_file).name
         filename_parsed = re.match(r'(\d{4}.\d.\d).(\d)_(.*)+_(\w+).zip', filename)
-        print(filename_parsed.group(0))
-        release_name = "{}.{}".format(filename_parsed.group(1), filename_parsed.group(2))
-        release_dir = "{}/{}".format(self.storage_path, release_name)
 
-        if not os.path.isdir(release_dir):
-            os.mkdir(release_dir)
+        release_name = "{}.{}".format(filename_parsed.group(1), filename_parsed.group(2))
+        release_dir = self.storage_path / (release_name)
+
+        if not release_dir.exists():
+            release_dir.mkdir()
 
         with zipfile.ZipFile(artifact_file, 'r') as zip_ref:
-            for file in zip_ref.namelist():
-                if file.endswith("master.manifest"):
-                    zip_ref.extract(file, "{}/temp".format(release_dir))
-                    if os.path.isfile("{}/sysupgrade/master.manifest".format(release_dir)):
-                        manifest = Manifest()
-                        manifest.load("{}/sysupgrade/master.manifest".format(release_dir))
-                        manifest_part = Manifest()
-                        manifest_part.load("{}/temp/master.manifest".format(release_dir))
-                        manifest.merge(manifest_part)
-                        manifest.export("{}/sysupgrade/master.manifest".format(release_dir))
+            for file in zip_ref.infolist():
+                if file.filename.endswith("master.manifest"):
 
-                if file.startswith('images/factory/'):
-                    zip_ref.extract(file, "{}/factory".format(release_dir))
-                elif file.startswith('images/sysupgrade/'):
-                    zip_ref.extract(file, "{}/sysupgrade".format(release_dir))
-                elif file.startswith('images/other/'):
-                    zip_ref.extract(file, "{}/other".format(release_dir))
+                    zip_ref.extract(file, release_dir / ("temp"))
+                    manifest_path = release_dir / "sysupgrade" / "master.manifest"
+                    if manifest_path.exists():
+                        manifest = Manifest()
+                        manifest.load(manifest_path)
+                        manifest_part = Manifest()
+                        manifest_part.load(release_dir / "temp" / "master.manifest")
+                        manifest.merge(manifest_part)
+                        manifest.export(manifest_path)
+
+                if file.filename.startswith('images/factory/'):
+                    file.filename = Path(file.filename).name
+                    zip_ref.extract(file, release_dir / "factory")
+                elif file.filename.startswith('images/sysupgrade/'):
+                    file.filename = Path(file.filename).name
+                    zip_ref.extract(file, release_dir / "sysupgrade")
+                elif file.filename.startswith('images/other/'):
+                    file.filename = Path(file.filename).name
+                    zip_ref.extract(file, release_dir / "other")
 
     def delete(self, artifact_file):
         return True
