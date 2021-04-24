95035109f1ec14273e2f01db19c755858fdb21c9
Benjamin Brahmer <info@b-brahmer.de>
2021-04-24 14:10:13 +0200

    add automation.py

---
+++ automation.py
@@ -0,0 +1,133 @@
+#! python3
+import argparse
+import os
+import re
+from pathlib import Path
+from progress.bar import Bar
+from firmware_collector.api import API
+from firmware_collector.repository import Repository
+from firmware_collector.storage import Storage
+from firmware_collector.manifest import Manifest
+
+
+class Collector():
+    def __init__(self):
+        super().__init__()
+        try:
+            self.config = {}
+            self.config["url"] = os.environ['ffsh_url']
+            self.config["username"] = os.environ['ffsh_username']
+            self.config["secret"] = os.environ['ffsh_secret']
+            self.config["db_path"] = "/tmp/ffsh/database_store"
+            self.config["download_path"] = "/tmp/ffsh/download_store"
+            self.config["firmware_path"] = "/tmp/ffsh/firmware_store"
+        except KeyError as e:
+            print("You are missing a environment variable {}".format(e))
+
+    def update(self):
+        """
+        updates the entries in the DB
+        """
+        api = API(self.config["url"], self.config["username"], self.config["secret"])
+        repository = Repository(Path(self.config["db_path"]) / "repo.db")
+        api.load_artifacts()
+
+        artifact_list = api.get_artifact_ids()
+        bar = Bar('Processing', max=len(artifact_list))
+
+        for artifact_id in artifact_list:
+            repository.create(api.get_artifact(artifact_id))
+            bar.next()
+        bar.finish()
+
+    def __match(self, artifact, version):
+        """
+        checks if this file should be downloaded
+        """
+        if not artifact.stored and artifact.name.endswith("output") and version in artifact.name:
+            return True
+
+    def download(self, version):
+        """
+        downloads all the artifacts, that are not yet downloaded"
+        """
+        api = API(self.config["url"], self.config["username"], self.config["secret"])
+        repository = Repository(Path(self.config["db_path"]) / "repo.db")
+
+        download_list = []
+        for a_id in repository.read_all_id():
+            artifact = repository.read(a_id)
+            if self.__match(artifact, version):
+                download_list.append(artifact)
+
+        bar = Bar('Processing', max=len(download_list))
+        for artifact in download_list:
+            bar.next()
+            try:
+                api.download_artifact(artifact, self.config["download_path"])
+                artifact.stored = True
+                repository.update(artifact.id, artifact)
+            except Exception as exception:
+                print("Download failed {}".format(exception))
+        bar.finish()
+
+    def store(self):
+        """
+        stores the images from the download dir in a proper way
+        """
+        # repository = Repository(self.config["db_path"])
+        storage = Storage(self.config["firmware_path"])
+        file_list = [file for file in os.scandir(self.config["download_path"])]
+
+        bar = Bar('Processing', max=len(file_list))
+        for artifact in file_list:
+            bar.next()
+            storage.save(artifact.path)
+        bar.finish()
+
+    def manifest(self, version, branch):
+        version_pattern = re.compile(r'(\d{4}.\d.\d).(\d)')
+        if version_pattern.match(version) and branch is not None:
+            manifest = Manifest()
+            manifest_path = Path(self.config["firmware_path"]) / version / "sysupgrade" / "stable.manifest"
+            manifest.load(manifest_path)
+            manifest.set_branch(branch)
+            manifest.export(manifest_path.parent / "{}.{}".format(branch, "manifest"))
+
+    def create_stores(self):
+        Path(self.config["db_path"]).mkdir(parents=True, exist_ok=True)
+        Path(self.config["download_path"]).mkdir(parents=True, exist_ok=True)
+        Path(self.config["firmware_path"]).mkdir(parents=True, exist_ok=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This is the firmware-collector automation interface')
+    parse_group = parser.add_mutually_exclusive_group()
+    parse_group.add_argument('--update', action="store_true", default=False)
+    parse_group.add_argument('--download', action="store", default=False)
+    parse_group.add_argument('--store', action="store_true", default=False)
+    parse_group.add_argument('--manifest', action="store", default=False)
+    parser.add_argument("--branch", action="store", required=False)
+    args = parser.parse_args()
+
+    try:
+        collector = Collector()
+        try:
+            collector.create_stores()
+        except Exception as e:
+            print("Can't create directories {}".format(e))
+        if args.update:
+            collector.update()
+        elif args.download:
+            if args.download == "":
+                print("Please provide a version.")
+            else:
+                collector.download(args.download)
+        elif args.store:
+            collector.store()
+        elif args.manifest:
+            collector.manifest(args.manifest, args.branch)
+    except ModuleNotFoundError as module_error:
+        print("Did you forget to activate the virtual-env?")
+        print("Error: {}".format(module_error))
+        print("Maybe run: . venv/bin/activate")
